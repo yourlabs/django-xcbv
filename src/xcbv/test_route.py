c8383e334614f43c7e61d5734041e92c29d58719
jpic <jamespic@gmail.com>
2018-03-11 16:38:34 +0100

    wip

--- src/xcbv/test_route.py
+++ src/xcbv/test_route.py
@@ -1,7 +1,15 @@
 import pytest
 
 
-class RouterNamespaceNotResolvable(Exception):
+class xcbvException(Exception):
+    pass
+
+
+class RouterNamespaceNotResolvable(xcbvException):
+    pass
+
+
+class NamespaceCollision(xcbvException):
     pass
 
 
@@ -32,6 +40,10 @@ class Router(object):
         for key, value in state.items():
             setattr(self, key, value)
 
+        self.namespace_init()
+        self.children_init()
+
+    def namespace_init(self):
         namespace = getattr(self, 'namespace', None)
         if not namespace:
             app_name = getattr(self, 'app_name', None)
@@ -40,6 +52,7 @@ class Router(object):
             else:
                 raise RouterNamespaceNotResolvable()
 
+    def children_init(self):
         children = list(self.children) + list(children)
         self.children = []
         for child in children:
@@ -47,10 +60,18 @@ class Router(object):
                 if issubclass(child, Route):
                     self.children.append(child.factory(parent=self))
                 elif issubclass(child, Router):
-                    self.children.append(Router(parent=self))
+                    self.children.append(child(parent=self))
             elif isinstance(child, Router):
                 child.parent = self
                 self.children.append(child)
+
+        namespaces = []
+        for child in self.children:
+            if not isinstance(child, Router):
+                continue
+            if child.namespace in namespaces:
+                raise NamespaceCollision()
+            namespaces.append(child.namespace)
 
 
 def test_router_init():
@@ -83,6 +104,29 @@ def test_router_override_parent_namespace():
     r = Router(Router(namespace='b'), namespace='a')
     assert r.namespace == 'a'
     assert r.children[0].namespace == 'b'
+
+
+def test_router_with_namespace_as_classattr():
+    class Test(Router):
+        namespace = 'b'
+
+    r = Test(Test)
+    assert r.namespace == 'b'
+    assert r.children[0].namespace == 'b'
+
+    r = Test(Test, namespace='a')
+    assert r.namespace == 'a'
+    assert r.children[0].namespace == 'b'
+
+    r = Test(Test(namespace='a'))
+    assert r.namespace == 'b'
+    assert r.children[0].namespace == 'a'
+
+
+def test_router_namespace_clash():
+    r = Router(namespace='a')
+    with pytest.raises(NamespaceCollision):
+        r = Router(r, r, namespace='a')
 
 
 def test_router_with_app_name():
